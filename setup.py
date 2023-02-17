5f38c227ea873aa1eb931268b0b0204e53204dce
Ricky Huang <rickypc@users.noreply.github.com>
2023-02-17 09:00:51 -0800

    Use setup.cfg.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #    Amazon DynamoDB SQL Library - an Amazon DynamoDB testing library with SQL-like DSL.
-#    Copyright (C) 2014 - 2015  Richard Huang <rickypc@users.noreply.github.com>
+#    Copyright (C) 2014 - 2023  Richard Huang <rickypc@users.noreply.github.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -21,40 +21,7 @@
 Amazon DynamoDB SQL Library - an Amazon DynamoDB testing library with SQL-like DSL.
 """
 
-# To use a consistent encoding
-import codecs
-from os.path import abspath, dirname, join
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-from src.DynamoDBSQLLibrary.version import VERSION
+from setuptools import setup
 
-LIBRARY_NAME = 'DynamoDBSQLLibrary'
-CWD = abspath(dirname(__file__))
-
-with codecs.open(join(CWD, 'README.rst'), encoding='utf-8') as reader:
-    LONG_DESCRIPTION = reader.read()
-
-setup(
-    name='robotframework-%s' % LIBRARY_NAME.lower(),
-    version=VERSION,  # pylint: disable=undefined-variable  # noqa
-    description='An Amazon AWS DynamoDB big data testing library for Robot Framework '
-                'with SQL-like DSL',
-    long_description=LONG_DESCRIPTION,
-    url='https://github.com/rickypc/robotframework-%s' % LIBRARY_NAME.lower(),
-    author='Richard Huang',
-    author_email='rickypc@users.noreply.github.com',
-    license='AGPL 3',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Programming Language :: Python :: 2.7',
-    ],
-    keywords='robot framework testing automation bigdata amazon dynamodb dynamo '
-             'db nosql sql dsl json aws big data softwaretesting',
-    platforms='any',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=['boto3 >= 1.1.1', 'dql >= 0.4.1', 'robotframework >= 2.6.0']
-)
+setup()
